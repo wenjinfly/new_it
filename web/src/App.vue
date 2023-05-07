e4a1cbdcc2829c18c5fa2dccba19d07361b35bbc
wenjinfly <wenjinfly@sina.com>
2023-05-08 00:21:38 +0800

    创建一个空的前端，明天开始写代码

---
+++ web/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+</template>
+
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+
+
+<style scoped></style>
